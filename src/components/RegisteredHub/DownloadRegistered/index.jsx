350b6fb0c203b106f1f9fdc9a8fe6dc0875ec466
Gabriel  Alves <68339733+gabbezeira@users.noreply.github.com>
2024-08-08 04:16:08 -0300

    masterpice version

---
+++ src/components/RegisteredHub/DownloadRegistered/index.jsx
@@ -0,0 +1,95 @@
+import { Container } from './styles'
+import { useContext, useState, useEffect } from 'react'
+import { AuthContext } from '../../../context/AuthContext'
+import { Loader } from '../../Loader'
+import { api } from '../../../services/api'
+import { NotificationContext } from '../../../context/NotificationContext'
+
+export function DownloadRegistered({ onClose }) {
+  const { loading } = useContext(AuthContext)
+  const [title, setTitle] = useState('')
+  const [image, setImage] = useState('')
+  const [link, setLink] = useState('')
+  const [description, setDescription] = useState('')
+
+  const { showSnackbar, snackbar } = useContext(NotificationContext)
+  const [isSnackbarActive, setIsSnackbarActive] = useState(false)
+
+  useEffect(() => {
+    setIsSnackbarActive(snackbar.show)
+  }, [snackbar])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const data = { title, image, link, description }
+    try {
+      await api.post('/files', data)
+      showSnackbar('Arquivo Cadastrado!', 'success')
+      onClose()
+    } catch (error) {
+      showSnackbar('Erro ao cadastrar arquivo:', error, 'erro')
+    }
+  }
+
+  return (
+    <Container>
+      <h1 className="title">Cadastrar arquivo</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-inputs">
+          <div className="duo-section">
+            <div className="input-area">
+              <h1 className="label">Nome do arquivo</h1>
+              <input
+                className="input-box"
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+            <div className="input-area">
+              <h1 className="label">Ícone do arquivo</h1>
+              <input
+                className="input-box"
+                type="text"
+                value={image}
+                onChange={(e) => setImage(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="input-area">
+            <h1 className="label">Link do arquivo</h1>
+            <input
+              className="input-box"
+              type="text"
+              value={link}
+              onChange={(e) => setLink(e.target.value)}
+            />
+          </div>
+
+          <div className="input-area">
+            <h1 className="label">Descrição completa</h1>
+            <textarea
+              className="text-area-box"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="buttons">
+          <button
+            className="button"
+            type="button"
+            onClick={onClose}
+            disabled={loading || isSnackbarActive}
+          >
+            {loading || isSnackbarActive ? <Loader size={24} /> : 'Cancelar'}
+          </button>
+          <button className="button colored" type="submit" disabled={loading}>
+            {loading ? <Loader size={24} /> : 'Cadastrar'}
+          </button>
+        </div>
+      </form>
+    </Container>
+  )
+}
--- src/components/RegisteredHub/index.jsx
+++ src/components/RegisteredHub/index.jsx
@@ -1,20 +1,43 @@
+import { useState } from 'react'
 import { Container } from './styles'
 import { Link } from 'react-router-dom'
 import { Video, FolderArchive } from 'lucide-react'
+import { DownloadRegistered } from './DownloadRegistered'
+import { VideoRegistered } from './VideoRegistered'
+import { Modal } from '../Modal'
 
 export function RegisteredHub() {
+  const [modalContent, setModalContent] = useState(null)
+
+  const openModal = (content) => {
+    setModalContent(content)
+  }
+
+  const closeModal = () => {
+    setModalContent(null)
+  }
+
   return (
     <Container>
       <div className="hub-area">
-        <Link className="hub-button" to="/register-hub">
+        <Link
+          className="hub-button"
+          to="/register-hub"
+          onClick={() => openModal(<VideoRegistered onClose={closeModal} />)}
+        >
           <Video className="icon" />
           <p className="text">Vídeos Cadastrados</p>
         </Link>
-        <Link className="hub-button" to="/register-hub">
+        <Link
+          className="hub-button"
+          to="/register-hub"
+          onClick={() => openModal(<DownloadRegistered onClose={closeModal} />)}
+        >
           <FolderArchive className="icon" />
           <p className="text">Downloads Cadastrados</p>
         </Link>
       </div>
+      {modalContent && <Modal onClose={closeModal}>{modalContent}</Modal>}
     </Container>
   )
 }
